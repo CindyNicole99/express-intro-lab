e0b2567527fc0f325757f7e30668a72cc0c81b7d
CindyNicole99 <salcidocindy99@yahoo.com>
2022-06-13 19:25:34 -0500

    complete lab

---
+++ data/students-data.js
@@ -0,0 +1,19 @@
+const students = [
+    {name: 'Cindy', city: 'HTX', texas: true},
+    {name: 'Kailana', city: 'GTX', texas: true},
+    {name: 'Savienne', city: 'TBD', texas: false},
+    {name: 'Coco', city: 'NYC', texas: false},
+    {name: 'Rachel', city: 'SF', texas: false},
+    {name: 'Kathy', city: 'BLM', texas: false},
+    {name: 'Minji', city: 'LA', texas: false},
+    {name: 'Jordin', city: 'BOS', texas: false},
+    {name: 'Beryl', city: 'DUR', texas: false },
+    {name: 'Dahlia', city: 'ATX', texas: true},
+    {name: 'Alexandrea', city: 'ATL', texas: false},
+    {name: 'Haydee', city: 'BAY', texas: false},
+    {name: 'Ilyana', city: 'FWTX', texas: true}
+]
+
+export {
+    students
+}
